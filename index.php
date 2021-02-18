8ab96052215c0c6e651ce09407e1a92501bd77e6
Fadil <mhmmdfadilah8@gmail.com>
2021-02-18 19:26:35 +0700

    praktikum 02 membuat objek dan class

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+include("Mahasiswa.php");
+
+$fadilah = new Mahasiswa();
+$eraldo = new Mahasiswa();
+$harry = new Mahasiswa();
+
+$fadilah->nim = 'H1101191034';
+$fadilah->nama = 'Muhammad Fadilah';
+$fadilah->tanggal_lahir = '3 Januari 2001';
+$fadilah->jenis_kelamin = 'Laki-laki';
+
+$eraldo->nim = 'H1101191026';
+$eraldo->nama = 'Eraldo Jonathan Ramadhan';
+$eraldo->tanggal_lahir = '18 Desember 2000';
+$eraldo->jenis_kelamin = 'Laki-laki';
+
+$harry->nim = 'H1101191040';
+$harry->nama = 'Harry Anshary';
+$harry->tanggal_lahir = '31 Maret 2001';
+$harry->jenis_kelamin = 'Laki-laki';
+
+$fadilah->tampilkanAngkatan();
+$fadilah->tampilkanUmur();
+$eraldo->tampilkanAngkatan();
+$eraldo->tampilkanUmur();
+$harry->tampilkanAngkatan();
+$harry->tampilkanUmur();
