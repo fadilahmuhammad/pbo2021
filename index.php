138c043c17802ff308b8df9deadfa4b811808264
Fadil <mhmmdfadilah8@gmail.com>
2021-02-28 20:11:03 +0700

    Praktikum 03 - Membuat class User, MahasiswaBaru, Pegawai dan Dosen

--- index.php
+++ index.php
@@ -1,22 +1,50 @@
 <?php
+require_once ('Mahasiswa.php');
+require_once ('MahasiswaBaru.php');
+require_once ('Pegawai.php');
+require_once ('Dosen.php');
+require_once ('User.php');
 
-include("Mahasiswa.php");
+$fadilah = new Mahasiswa('H1101191034','Muhammad Fadilah','2001-01-03','Pria');
+$eraldo = new Mahasiswa('H1101191026','Eraldo Jonathan Ramadhan','2000-12-18','Pria');
+$harry = new Mahasiswa('H1101191040','Harry Anshary','2001-03-31','Pria');
 
-$fadilah = new Mahasiswa();
-$eraldo = new Mahasiswa();
-$harry = new Mahasiswa();
+$pegawai1 = new Pegawai('1234567','Pegawai Satu','086512347865','Rp. 4.500.000');
+$pegawai2 = new Pegawai('4567891','Pegawai Dua','089987653421','Rp. 5.500.000');
 
-$fadilah->nim = 'H1101191034';
-$fadilah->nama = 'Muhammad Fadilah';
-$fadilah->tanggal_lahir = '3 Januari 2001';
-$fadilah->jenis_kelamin = 'Laki-laki';
+$maba1 = new MahasiswaBaru('H1101201069','Ramdani Kurniawan','2001-09-04','Pria', 678895);
+$maba2 = new MahasiswaBaru('H1101201088','Regi Prabowo','2001-08-07','Pria', 558769);
 
-$eraldo->nim = 'H1101191026';
-$eraldo->nama = 'Eraldo Jonathan Ramadhan';
-$eraldo->tanggal_lahir = '18 Desember 2000';
-$eraldo->jenis_kelamin = 'Laki-laki';
+$dosen = new Dosen('8863453','Dosen Satu','086512347865','Rp. 4.500.000','456678','Pengajar');
+$dosen = new Dosen('9928919','Dosen Dua','089987653421','Rp. 5.500.000','567899', 'Ketua Jurusan');
 
-$harry->nim = 'H1101191040';
-$harry->nama = 'Harry Anshary';
-$harry->tanggal_lahir = '31 Maret 2001';
-$harry->jenis_kelamin = 'Laki-laki';
+$user1 = new User('user1','user1');
+$user2 = new User('user2','user2');
+?>
+
+<html>
+    <head>
+        <title>Inheritance</title>
+    </head>
+    <body>
+    
+    <?php 
+        $fadilah->tampilkanAngkatan();
+        $fadilah->tampilkanUmur();
+        $fadilah->tampilkanNama();
+    ?>
+
+    <?php 
+        $eraldo->tampilkanAngkatan();
+        $eraldo->tampilkanUmur();
+        $eraldo->tampilkanNama(); 
+    ?>
+
+    <?php
+        $harry->tampilkanAngkatan();
+        $harry->tampilkanUmur();
+        $harry->tampilkanNama();
+    ?>
+
+    </body>
+</html>
--- mahasiswa.php
+++ mahasiswa.php
@@ -1,19 +1,30 @@
 <?php
-
-class Mahasiswa 
-{
-    public $nim;
-    public $nama;
-    public $tanggal_lahir;
-    public $jenis_kelamin;
-
-    public function tampilkanAngkatan()
-    {
-
-    }
-
-    public function tampilkanUmur()
-    {
-        
-    }
+require_once ('User.php');
+class Mahasiswa extends User {
+ public $nim;
+ public $nama;
+ public $tanggal_lahir;
+ public $jenis_kelamin;
+ 
+ function __construct($nim,$nama,$tgl,$jk){
+   $this->nim = $nim;
+   $this->nama = $nama;
+   $this->tanggal_lahir = $tgl;
+   $this->jenis_kelamin = $jk;
+ }
+ 
+ public function tampilkanAngkatan(){
+  echo 'Angkatan : '. substr($this->nim,5,-4). '<br>';
+ }
+ 
+ public function tampilkanUmur(){
+   echo 'Umur : '. date_diff(date_create($this->tanggal_lahir), date_create('today'))->y. '<br>';
+ }
+ 
+ public function tampilkanNama(){
+     echo 'Nama : '.$this->nama. '<br>'. '<br>';
+ }
+ 
 }
+?>
+
